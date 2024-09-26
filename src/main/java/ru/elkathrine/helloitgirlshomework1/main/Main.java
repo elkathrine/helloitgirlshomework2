93d884aa135d4bb3b7a599696fbb8ae92c778fb9
elkathrine <leonteva-kate@yandex.com>
2024-09-27 01:14:52 +0300

    Changed

--- src/main/java/ru/elkathrine/helloitgirlshomework1/main/Main.java
+++ src/main/java/ru/elkathrine/helloitgirlshomework1/main/Main.java
@@ -1,4 +1,20 @@
 package ru.elkathrine.helloitgirlshomework1.main;
 
 public class Main {
+
+    public static void main(String[] args) {
+        String x;
+        x = "Katya";
+        sayMerhaba(x);
+
+        String y;
+        y = "Nikita";
+       sayMerhaba(y);
+
+        System.out.println("Iyi geceler");
+    }
+
+    public static void sayMerhaba(String name) {
+        System.out.println("Merhaba, " + name);
+    }
 }
